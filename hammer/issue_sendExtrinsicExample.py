7dcb027c86418879a14b0a12ce6a1eddd247eeca
Andreas Krueger <coding@AndreasKrueger.de>
2020-06-18 10:36:31 +0200

    polkascan answered, library is substrate version specific right now

--- hammer/issue_sendExtrinsicExample.py
+++ hammer/issue_sendExtrinsicExample.py
@@ -8,21 +8,29 @@
 @see:     https://github.com/drandreaskrueger/chainhammer-substrate for updates
 """
 
-from substrateinterface import SubstrateInterface, Keypair, SubstrateRequestException
-from pprint import pformat
-substrate = SubstrateInterface( url="ws://127.0.0.1:9944" )
-keypair = Keypair.create_from_mnemonic('episode together nose spoon dose oil faculty zoo ankle evoke admit walnut')
-print ("sending from:", keypair.ss58_address)
-BOB_ADDRESS = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty'
-call = substrate.compose_call(
-    call_module='Balances', call_function='transfer',
-    call_params={
-        'dest': BOB_ADDRESS,
-        'value': 1 * 10**12})
-try:
-    extrinsic = substrate.create_signed_extrinsic(call=call, keypair=keypair)
-    result = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
-    print("Extrinsic '{}' sent and included in block '{}'".format(result['extrinsic_hash'], result['block_hash']))
-except (SubstrateRequestException, ValueError) as e:
-    print("Failed to send: {} with args:".format(type(e)))
-    print("{}".format(pformat(e.args[0])))
+def errorenous():
+    """
+    answered here https://github.com/polkascan/py-substrate-interface/issues/14
+    this would work but only with newer substrate version than rc2
+    """
+    from substrateinterface import SubstrateInterface, Keypair, SubstrateRequestException
+    from pprint import pformat
+    substrate = SubstrateInterface( url="ws://127.0.0.1:9944" )
+    keypair = Keypair.create_from_mnemonic('episode together nose spoon dose oil faculty zoo ankle evoke admit walnut')
+    print ("sending from:", keypair.ss58_address)
+    BOB_ADDRESS = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty'
+    call = substrate.compose_call(
+        call_module='Balances', call_function='transfer',
+        call_params={
+            'dest': BOB_ADDRESS,
+            'value': 1 * 10**12})
+    try:
+        extrinsic = substrate.create_signed_extrinsic(call=call, keypair=keypair)
+        result = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
+        print("Extrinsic '{}' sent and included in block '{}'".format(result['extrinsic_hash'], result['block_hash']))
+    except (SubstrateRequestException, ValueError) as e:
+        print("Failed to send: {} with args:".format(type(e)))
+        print("{}".format(pformat(e.args[0])))
+
+if __name__ == '__main__':
+    errorenous()
