aba8142aa6976ba0f085f6ed230e9057d7459c6c
Andreas Krueger <coding@AndreasKrueger.de>
2020-06-25 13:36:57 +0200

    simplified, clarified; added quickstart; asked in riot channel

--- hammer/issue_sendExtrinsicExample.py
+++ hammer/issue_sendExtrinsicExample.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 """
-@summary: send extrinsic - how to?
-          found an error, reported in https://github.com/polkascan/py-substrate-interface/issues/14
-          output results = see bottom of this file
+@summary: Send extrinsic - how to? Example doesn't work yet for substrate & node-template.
+          For reporting in https://github.com/polkascan/py-substrate-interface/issues/14
+          Output RESULTS = see bottom of this file
 
-@version: v04 (18/June/2020)
+@version: v05 (25/June/2020)
 @since:   10/June/2020
 @author:  https://github.com/drandreaskrueger
 @see:     https://github.com/drandreaskrueger/chainhammer-substrate for updates
 
-@attention: versions!
-            pip uninstall scalecodec substrateinterface substrate-interface;
-            pip install scalecodec substrate-interface
-            pip freeze | grep "scalecodec\|substrate"
+@attention: versions:
+                pip freeze | grep "scalecodec\|substrate"
+                    scalecodec==0.9.55
+                    substrate-interface==0.9.14
 
-scalecodec==0.9.54
-substrate-interface==0.9.14
+@quickstart:
+          python3 -m venv env
+          source env/bin/activate
+          pip3 install --upgrade pip
+          pip3 install substrate-interface
+          wget https://gist.githubusercontent.com/drandreaskrueger/053bfe0c8ddfaeeb50134a977708f999/raw -O send_extrinsic.py
+
+          node-template --dev
+          python3 send_extrinsic.py 
 """
 
 from substrateinterface import SubstrateInterface, Keypair, SubstrateRequestException
 from pprint import pformat, pprint
 
-URL =   "ws://127.0.0.1:9944"
+# URL =   "ws://127.0.0.1:9944"
 URL = "http://127.0.0.1:9933"
 
 def compose_sign_and_send_extrinsic(substrate):
@@ -39,7 +46,7 @@ def compose_sign_and_send_extrinsic(substrate):
     try:
         extrinsic = substrate.create_signed_extrinsic(call=call, keypair=keypair)
         # result = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True) # works only with ws not http
-        result = substrate.submit_extrinsic(extrinsic) # works with http AND ws
+        result = substrate.submit_extrinsic(extrinsic) # works with both http AND ws
         print("Extrinsic '{}' sent and included in block '{}'".format(result['extrinsic_hash'], result.get('block_hash', None)))
         
     except (SubstrateRequestException, ValueError) as e:
@@ -47,8 +54,7 @@ def compose_sign_and_send_extrinsic(substrate):
         print("{}".format(pformat(e.args[0])))
 
 
-def with_custom_type_registry():
-    custom_type_registry = {  "runtime_id": 1,
+custom_type_registry_1 = {  "runtime_id": 1,
                               "types": {
                                 "Address": "AccountIdAddress",
                                 "ExtrinsicPayloadValue": {
@@ -67,6 +73,12 @@ def with_custom_type_registry():
                               "versioning": [
                               ]
                             }
+
+custom_type_registry_2 = {"please help" : "thanks."}
+
+
+def with_custom_type_registry(custom_type_registry):
+
     substrate = SubstrateInterface(url=URL,
                                    address_type=42, 
                                    type_registry_preset='default', 
@@ -76,11 +88,9 @@ def with_custom_type_registry():
 
 def print_getRuntimeVersion(substrate):
     print ("get_version() = %s" % substrate.get_version())
-    # AttributeError: 'SubstrateInterface' object has no attribute 'chain_getRuntimeVersion'
-    # grv = substrate.chain_getRuntimeVersion()
     grv=substrate.rpc_request(method="chain_getRuntimeVersion", params=[])
-    print ("chain_getRuntimeVersion():")
-    pprint(grv)
+    print ("chain_getRuntimeVersion()['result']:")
+    pprint(grv['result'])
            
 
 if __name__ == '__main__':
@@ -92,35 +102,37 @@ if __name__ == '__main__':
     compose_sign_and_send_extrinsic(substrate)
     
     print ("\nSubstrateInterface with custom type registry:")
-    substrate=with_custom_type_registry()
+    substrate=with_custom_type_registry(custom_type_registry_1)
     compose_sign_and_send_extrinsic(substrate)
 
     
 """
-# RESULT:
-# first:  node-template --dev
-# second: substrate --dev
+######################################
+# RESULTS:
+#        first:  node-template --dev
+#        second: substrate --dev
+######################################
 
+#########################
 ### node-template --dev
+#########################
 
 get_version() = 2.0.0-rc3-f5acce1-x86_64-linux-gnu
-chain_getRuntimeVersion():
-{'id': 1,
- 'jsonrpc': '2.0',
- 'result': {'apis': [['0xdf6acb689907609b', 3],
-                     ['0x37e397fc7c91f5e4', 1],
-                     ['0x40fe3ad401f8959a', 4],
-                     ['0xd2bc9897eed08f15', 2],
-                     ['0xf78b278be53f454c', 2],
-                     ['0xdd718d5cc53262d4', 1],
-                     ['0xab3c0572291feb8b', 1],
-                     ['0xed99c5acb25eedf5', 2]],
-            'authoringVersion': 1,
-            'implName': 'node-template',
-            'implVersion': 1,
-            'specName': 'node-template',
-            'specVersion': 1,
-            'transactionVersion': 1}}
+chain_getRuntimeVersion()['result']:
+{'apis': [['0xdf6acb689907609b', 3],
+          ['0x37e397fc7c91f5e4', 1],
+          ['0x40fe3ad401f8959a', 4],
+          ['0xd2bc9897eed08f15', 2],
+          ['0xf78b278be53f454c', 2],
+          ['0xdd718d5cc53262d4', 1],
+          ['0xab3c0572291feb8b', 1],
+          ['0xed99c5acb25eedf5', 2]],
+ 'authoringVersion': 1,
+ 'implName': 'node-template',
+ 'implVersion': 1,
+ 'specName': 'node-template',
+ 'specVersion': 1,
+ 'transactionVersion': 1}
 
 Default SubstrateInterface:
 sending from: 5HmubXCdmtEvKmvqjJ7fXkxhPXcg6JTS62kMMphqxpEE6zcG
@@ -139,30 +151,30 @@ Failed to send: <class 'substrateinterface.exceptions.SubstrateRequestException'
 {'code': 1010, 'data': 'BadProof', 'message': 'Invalid Transaction'}
 
 
+#####################
 ### substrate --dev
+#####################
 
 get_version() = 2.0.0-rc3-34695a856-x86_64-linux-gnu
-chain_getRuntimeVersion():
-{'id': 1,
- 'jsonrpc': '2.0',
- 'result': {'apis': [['0xdf6acb689907609b', 3],
-                     ['0x37e397fc7c91f5e4', 1],
-                     ['0x40fe3ad401f8959a', 4],
-                     ['0xd2bc9897eed08f15', 2],
-                     ['0xf78b278be53f454c', 2],
-                     ['0xed99c5acb25eedf5', 2],
-                     ['0xcbca25e39f142387', 2],
-                     ['0x687ad44ad37f03c2', 1],
-                     ['0xbc9d89904f5b923f', 1],
-                     ['0x68b66ba122c93fa7', 1],
-                     ['0x37c8bb1350a9a2a8', 1],
-                     ['0xab3c0572291feb8b', 1]],
-            'authoringVersion': 10,
-            'implName': 'substrate-node',
-            'implVersion': 2,
-            'specName': 'node',
-            'specVersion': 251,
-            'transactionVersion': 1}}
+chain_getRuntimeVersion()['result']:
+{'apis': [['0xdf6acb689907609b', 3],
+          ['0x37e397fc7c91f5e4', 1],
+          ['0x40fe3ad401f8959a', 4],
+          ['0xd2bc9897eed08f15', 2],
+          ['0xf78b278be53f454c', 2],
+          ['0xed99c5acb25eedf5', 2],
+          ['0xcbca25e39f142387', 2],
+          ['0x687ad44ad37f03c2', 1],
+          ['0xbc9d89904f5b923f', 1],
+          ['0x68b66ba122c93fa7', 1],
+          ['0x37c8bb1350a9a2a8', 1],
+          ['0xab3c0572291feb8b', 1]],
+ 'authoringVersion': 10,
+ 'implName': 'substrate-node',
+ 'implVersion': 2,
+ 'specName': 'node',
+ 'specVersion': 251,
+ 'transactionVersion': 1}
 
 Default SubstrateInterface:
 sending from: 5HmubXCdmtEvKmvqjJ7fXkxhPXcg6JTS62kMMphqxpEE6zcG
